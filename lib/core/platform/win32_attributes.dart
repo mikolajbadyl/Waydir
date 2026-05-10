de6e6d64a0389ce14b2aee233175c2fd4aa05662
Mikołaj Badyl <mikolajbadyl0@gmail.com>
2026-05-10 22:06:53 +0200

    fix: ShellExecuteW takes 6 params not 5, add missing lpDirectory

--- lib/core/platform/win32_attributes.dart
+++ lib/core/platform/win32_attributes.dart
@@ -10,8 +10,14 @@ DynamicLibrary? _kernel32;
 int Function(Pointer<Utf16>)? _getFileAttributesW;
 
 DynamicLibrary? _shell32;
-int Function(int, Pointer<Utf16>, Pointer<Utf16>, Pointer<Utf16>, int)?
-_shellExecuteW;
+int Function(
+  int,
+  Pointer<Utf16>,
+  Pointer<Utf16>,
+  Pointer<Utf16>,
+  Pointer<Utf16>,
+  int,
+)? _shellExecuteW;
 
 void _ensureKernel32() {
   if (_kernel32 != null) return;
@@ -28,15 +34,10 @@ void _ensureShell32() {
   _shell32 = DynamicLibrary.open('shell32.dll');
   _shellExecuteW = _shell32!
       .lookupFunction<
-        IntPtr Function(
-          IntPtr,
-          Pointer<Utf16>,
-          Pointer<Utf16>,
-          Pointer<Utf16>,
-          Int32,
-        ),
-        int Function(int, Pointer<Utf16>, Pointer<Utf16>, Pointer<Utf16>, int)
-      >('ShellExecuteW');
+          IntPtr Function(IntPtr, Pointer<Utf16>, Pointer<Utf16>,
+              Pointer<Utf16>, Pointer<Utf16>, Int32),
+          int Function(int, Pointer<Utf16>, Pointer<Utf16>, Pointer<Utf16>,
+              Pointer<Utf16>, int)>('ShellExecuteW');
 }
 
 bool isHiddenOnWindows(String path) {
@@ -59,7 +60,7 @@ void shellOpenOnWindows(String path) {
   final verb = 'open'.toNativeUtf16();
   final file = path.toNativeUtf16();
   try {
-    _shellExecuteW!(0, verb, file, nullptr, 1);
+    _shellExecuteW!(0, verb, file, nullptr, nullptr, 1);
   } finally {
     calloc.free(verb);
     calloc.free(file);
