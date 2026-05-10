8cb3e20db8d2cf3120f79ad823765c8492154185
Mikołaj Badyl <mikolajbadyl0@gmail.com>
2026-05-10 22:14:57 +0200

    chore: remove scaled_app and custom UI scaling system

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,21 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
-import 'package:scaled_app/scaled_app.dart';
 import 'app/waydir_app.dart';
 import 'core/fs/fs_worker_pool.dart';
 import 'core/settings/settings_store.dart';
-import 'core/system/system_scale.dart';
 import 'i18n/strings.g.dart';
 
 void main() async {
-  ScaledWidgetsFlutterBinding.ensureInitialized(scaleFactor: (_) => 1.0);
+  WidgetsFlutterBinding.ensureInitialized();
   LocaleSettings.useDeviceLocale();
   unawaited(FsWorkerPool.instance.ensureStarted());
   await SettingsStore.instance.load();
-  await SystemScale.instance.start();
   runApp(TranslationProvider(child: const WaydirApp()));
 
   doWhenWindowReady(() {
-    final s = SystemScale.instance.uiScale.value;
-    appWindow.minSize = Size(700 * s, 450 * s);
-    appWindow.size = Size(1100 * s, 700 * s);
+    appWindow.minSize = const Size(700, 450);
+    appWindow.size = const Size(1100, 700);
     appWindow.alignment = Alignment.center;
     appWindow.title = '';
     appWindow.show();
